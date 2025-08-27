0ef72ed7265f24e51bd85767ff20ebaf4ce868d2
Fabian Serna <fabianserna2013@gmail.com>
2025-08-27 13:26:11 -0500

    Install bootstrap-icons

--- RealEstateUI/src/pages/properties/index.tsx
+++ RealEstateUI/src/pages/properties/index.tsx
@@ -1,5 +1,6 @@
 export const PropertiesPage = () => {
     return <h1 className="text-3xl font-bold underline">
         Hello world!
+        <i className="bi bi-alarm" style={{ fontSize: "2rem", color: "cornflowerblue" }}></i>
     </h1>
 }
